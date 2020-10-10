394fb237ecaa384b8cd50aa5e9e6051f26272a89
LAPTOP-TV1OTG8M\yongw <yongwon0824@naver.com>
2020-10-11 05:32:19 +0900

    :memo:Add Login View

---
+++ app/src/main/java/com/example/outstagram/Lobby.kt
@@ -0,0 +1,16 @@
+package com.example.outstagram
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.lobby.*
+
+class Lobby : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.lobby)
+
+
+    }
+
+}
--- app/src/main/java/com/example/outstagram/MainActivity.kt
+++ app/src/main/java/com/example/outstagram/MainActivity.kt
@@ -1,31 +1,22 @@
 package com.example.outstagram
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.lobby.*
 
 class MainActivity : AppCompatActivity() {
+    var login: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        home_layout.setVisibility(View.VISIBLE)
-        search_layout.setVisibility(View.INVISIBLE)
-        notification_layout.setVisibility(View.INVISIBLE)
-        home_btn.setOnClickListener{
-            home_layout.setVisibility(View.VISIBLE)
-            search_layout.setVisibility(View.INVISIBLE)
-            notification_layout.setVisibility(View.INVISIBLE)
+
+        login_btn.setOnClickListener {
+            val intent = Intent(this, Lobby::class.java)
+            startActivity(intent)
         }
-        search_btn.setOnClickListener {
-            home_layout.setVisibility(View.INVISIBLE)
-            search_layout.setVisibility(View.VISIBLE)
-            notification_layout.setVisibility(View.INVISIBLE)
-        }
-        notification_btn.setOnClickListener {
-            home_layout.setVisibility(View.INVISIBLE)
-            search_layout.setVisibility(View.INVISIBLE)
-            notification_layout.setVisibility(View.VISIBLE)
-        }
+
     }
 }
