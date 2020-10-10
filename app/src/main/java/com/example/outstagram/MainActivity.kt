ed792c4cbe9c47e851da01b61b368df248bc731e
LAPTOP-TV1OTG8M\yongw <yongwon0824@naver.com>
2020-10-11 04:07:52 +0900

    :memo:Add Nav and FrameLayout

--- app/src/main/java/com/example/outstagram/MainActivity.kt
+++ app/src/main/java/com/example/outstagram/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.outstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        home_layout.setVisibility(View.VISIBLE)
+        search_layout.setVisibility(View.INVISIBLE)
+        notification_layout.setVisibility(View.INVISIBLE)
+        home_btn.setOnClickListener{
+            home_layout.setVisibility(View.VISIBLE)
+            search_layout.setVisibility(View.INVISIBLE)
+            notification_layout.setVisibility(View.INVISIBLE)
+        }
+        search_btn.setOnClickListener {
+            home_layout.setVisibility(View.INVISIBLE)
+            search_layout.setVisibility(View.VISIBLE)
+            notification_layout.setVisibility(View.INVISIBLE)
+        }
+        notification_btn.setOnClickListener {
+            home_layout.setVisibility(View.INVISIBLE)
+            search_layout.setVisibility(View.INVISIBLE)
+            notification_layout.setVisibility(View.VISIBLE)
+        }
     }
 }
